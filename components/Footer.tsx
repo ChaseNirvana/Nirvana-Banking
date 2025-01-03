1a99000e7010886972991978a917c6262ed92225
ChaseNirvana <171287505+ChaseNirvana@users.noreply.github.com>
2025-01-02 23:15:21 -0500

    Add Sentry security - sign in appears broken

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
     <footer className="footer">
       <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
         <p className="text-xl font=bhold text-gray-700">
-          {user.name[0]}
+          {user?.name[0]}
         </p>
       </div>
 
@@ -25,7 +25,7 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
           {user.name}
         </h1>
         <p className="text-14 truncate font-normal text-gray-600">
-          {user.email}
+          {user?.email}
         </p>
       </div>
 
